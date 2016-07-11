81dcf91ed08af5bd4ecfba8c8d6c7e39f6cba93a
Marcos Paulo <marcosisocram@gmail.com>
2016-07-11 14:35:49 -0300

    Trocar para TE123456785AA

--- test/rastreio.js
+++ test/rastreio.js
@@ -2,9 +2,9 @@ import test from 'ava';
 import {rastreio} from './../app/lib/rastreio';
 
 test('dados corretos', t => {
-	return rastreio(['JO488220248BR'], {resultado: 'ultimo', formato: 'json'})
+	return rastreio(['TE123456785AA'], {resultado: 'ultimo', formato: 'json'})
 	.then(data => {
-		t.regex(data.toUpperCase(), /OBJETO POSTADO/);
+		t.regex(data, /Objeto saiu para entrega/);
 	})
 	.catch(err => {
 		t.fail(err);
@@ -12,21 +12,21 @@ test('dados corretos', t => {
 });
 
 test('número de rastreamento errado', t => {
-	return rastreio(['JO488220245BR'], {resultado: 'ultimo', formato: 'json'})
+	return rastreio(['TE123456185AA'], {resultado: 'ultimo', formato: 'json'})
 	.then(data => {
 		t.regex(data, /Objeto n.o encontrado na base de dados dos Correios\./);
 	});
 });
-//
+
 test('resultado errado', t => {
-	return rastreio(['JO488220248BR'], {resultado: 'errado', formato: 'json'})
+	return rastreio(['TE123456785AA'], {resultado: 'errado', formato: 'json'})
 		.catch(err => {
 			t.regex(err, /TypeError: Esperado uma das opções u, t, todos ou ultimo como resultado/);
 		});
 });
 
 test('formato errado', t => {
-	return rastreio(['JO488220248BR'], {resultado: 'u', formato: 'errado'})
+	return rastreio(['TE123456785AA'], {resultado: 'u', formato: 'errado'})
 	.catch(err => {
 		t.regex(err, /TypeError: Esperado uma das opções xml, json ou humanize/);
 	});
